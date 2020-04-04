ff305f5405cec6965d99f3ffeb676604f4c3ff24
akseonov <konstantinaxeonov@gmail.com>
2020-04-05 04:09:09 +0500

    - доделал дз к 4 уроку

--- lesson_4/index.php
+++ lesson_4/index.php
@@ -1,4 +1,6 @@
 <?php
+define('DIR_IMG', './gallery_img');
+define('DIR_LOG', './logs');
 
 function renderTemplate($page, $arrImg = [], $arrHtml = [])
 {
@@ -7,23 +9,36 @@ function renderTemplate($page, $arrImg = [], $arrHtml = [])
     return ob_get_clean();
 }
 
-function renderArr ($arr) {
-    $dir = './gallery_img';
-    $scanDir = array_diff(scandir($dir), array('..', '.'));
-    var_dump($scanDir);
-    for ($i = 2; $i < count($scanDir) + 2; $i++) {
-        $dir = './gallery_img/' . $scanDir[$i];
-        var_dump($dir);
-        $scanDirImg = array_diff(scandir($dir), array('..', '.'));
-        var_dump(count($scanDirImg) + 2);
-        for ($j = 2; $j < count($scanDirImg) + 2; $j++) {
-            $arr[$i - 2][$j - 2] = $dir . "/" . $scanDirImg[$j];
+function logging () {
+    $day = date('G:i:s d:m:Y');
+    file_put_contents(DIR_LOG . "/log.txt", $day . "\r\n", FILE_APPEND);
+
+    $log = count(explode("\r\n", file_get_contents(DIR_LOG . "/log.txt")));
+    var_dump($log);
+    if ($log > 10) {
+        $dir = DIR_LOG . "/";
+        $count = count(scandir($dir)) - 2;
+        rename($dir . "log.txt", $dir . "log" . $count . ".txt");
+    }
+}
+
+function renderArr () {
+    $arr = [];
+    $scanDir = scandir(DIR_IMG);
+    for ($i = 0; $i <= count($scanDir); $i++) {
+        if (strlen($scanDir[$i]) > 2) {
+            $scanDirImg = scandir(DIR_IMG . "/" . $scanDir[$i]);
+            for ($j = 0; $j <= count($scanDirImg); $j++) {
+                if (strlen($scanDirImg[$j]) > 2) {
+                    $arr[$i - 2][$j - 2] = DIR_IMG . "/" . $scanDir[$i] . "/" . $scanDirImg[$j];
+                }
+            }
         }
     }
     return $arr;
 }
 
-$arrImg = renderArr($arrImg);
+$arrImg = renderArr();
 
 $gallery = renderTemplate('gallery', $arrImg);
 
@@ -31,4 +46,4 @@ $arrHtml = array($gallery);
 
 echo renderTemplate('main', $arrImg, $arrHtml);
 
-
+logging();
